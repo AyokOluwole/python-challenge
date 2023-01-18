881086404d45041ac0b98e14e8c422fbc1dc4373
Ayokunle Oluwole <oluwole_ayokunle@yahoo.com>
2023-01-17 23:21:30 -0500

    third commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,15 +8,15 @@ budget_data_csv = '/Users/ayokunleoluwole/Desktop/python-challenge/PyBank/Resour
 
 #print(budget_data_csv)
 
-#budget_data_csv = os.path.join('PyBank','Resources', 'budget_data.csv')
+#budget_data_csv = os.path.join('..', '..','Resources', 'budget_data.csv')
 
-
+profit_loss = []
 total_months = 0  
 month_count = []
 difference = []
 total_profit_loss = 0 
-greatest_increase = 0 
-greatest_decrease = 0 
+greatest_increase = [] 
+greatest_decrease = [] 
 total_ave_change = 0
 profit = 0
 
@@ -43,10 +43,26 @@ with open(budget_data_csv) as csv_file:
 # and then the average of those changes
         total_ave_change = int(row[1]) 
 
+        profit_loss.append(int(row[1]))
+
+    for i in range(1,len(profit_loss)):
+        difference.append(profit_loss[i]- profit_loss[i-1])
         
+        average = sum(difference) / len(difference)
+    #print(profit_loss)
+    #print(average)
+    
+    maxValue = max(row[1])
+
+    print(maxValue)
+    
+    
+    
+
     print("Financial Analysis")
     print ("----------------------------")
     print("Total Months: " + str(total_months))
     print(f"Total Profit/Losess: ${total_profit_loss}\n")
-    print(f"Average Change: ${total_ave_change}\n")
+    print(f"Average Change: ${average}\n")
+    
     
